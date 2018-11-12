719af26aad12d64b7f5bf2f2a09c2c1cad2d4e5a
Lucy Wall <lhwall@gmail.com>
2018-11-12 19:48:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function overWriteBestCustomer(){
   bestCustomer = "maybe bob"
 }
 
-function changeLeastFavorite(){
+function changeLeastFavoriteCustomer(){
   const leastFavoriteCustomer = "bob"
 }
